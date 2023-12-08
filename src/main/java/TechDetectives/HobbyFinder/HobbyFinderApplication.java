76c5a8602d31909d0cb53f527fedbd89d38e1d97
romeo <romeoquist@gmail.com>
2023-12-07 19:52:50 -0600

    added spring framework

---
+++ src/main/java/TechDetectives/HobbyFinder/HobbyFinderApplication.java
@@ -0,0 +1,13 @@
+package TechDetectives.HobbyFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbyFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HobbyFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TechDetectives/HobbyFinder/HobbyFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package TechDetectives.HobbyFinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbyFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
