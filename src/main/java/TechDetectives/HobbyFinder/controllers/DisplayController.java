c989ae7083a1c2024f992704c694ca6de5a46941
Srijana061 <srijanabaral94@gmail.com>
2023-12-12 10:54:35 -0600

    added DisplayController

--- src/main/java/TechDetectives/HobbyFinder/controllers/DisplayController.java
+++ src/main/java/TechDetectives/HobbyFinder/controllers/DisplayController.java
@@ -1,2 +1,18 @@
-package TechDetectives.HobbyFinder.controllers;public class DisplayController {
+package TechDetectives.HobbyFinder.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DisplayController {
+    @GetMapping("/")
+
+    public String renderHomePage(Model model) {
+        model.addAttribute("headingText", "Welcome to Hobby Finder");
+
+        return "index" ;
+    }
+
 }
