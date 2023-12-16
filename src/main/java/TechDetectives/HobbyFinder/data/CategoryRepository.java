ac4ddabc5a384e5e7521da2b8ca3770dc5f15ac2
Srijana061 <srijanabaral94@gmail.com>
2023-12-15 20:47:54 -0600

    added CategoryRepository interface

--- src/main/java/TechDetectives/HobbyFinder/data/CategoryRepository.java
+++ src/main/java/TechDetectives/HobbyFinder/data/CategoryRepository.java
@@ -1,7 +1,10 @@
 package TechDetectives.HobbyFinder.data;
 
+import TechDetectives.HobbyFinder.models.Category;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-public interface HobbyRepository extends CrudRepository {
+
+public interface CategoryRepository extends CrudRepository<Category, Integer> {
+    Iterable<Category>findAllOrderByName(String name);
 }
