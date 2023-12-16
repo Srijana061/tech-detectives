3733c5ba8f4a523674fab0d57e85ec7ab3845da6
Srijana061 <srijanabaral94@gmail.com>
2023-12-15 20:50:05 -0600

    added Category class

--- src/main/java/TechDetectives/HobbyFinder/models/Category.java
+++ src/main/java/TechDetectives/HobbyFinder/models/Category.java
@@ -1,2 +1,56 @@
-package TechDetectives.HobbyFinder.models;public class Category {
+package TechDetectives.HobbyFinder.models;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String name;
+
+    @OneToMany
+    @JoinColumn(name= "category_id")
+    private final List<Hobby> hobbies= new ArrayList<>();
+
+    public Category() {
+
+    }
+
+
+    public Category(String name) {
+
+        this.name = name;
+
+    }
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return id == category.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
