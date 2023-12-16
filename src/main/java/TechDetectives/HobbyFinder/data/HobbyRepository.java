2e7eef524f9de3316e956c6d4c38fdc5c02ac81d
Srijana061 <srijanabaral94@gmail.com>
2023-12-15 20:48:45 -0600

    added HobbyRepository interface

--- src/main/java/TechDetectives/HobbyFinder/data/HobbyRepository.java
+++ src/main/java/TechDetectives/HobbyFinder/data/HobbyRepository.java
@@ -1,2 +1,10 @@
-package TechDetectives.HobbyFinder.data;public interface Hobby {
+package TechDetectives.HobbyFinder.data;
+
+
+import TechDetectives.HobbyFinder.models.Hobby;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HobbyRepository extends CrudRepository<Hobby, Integer> {
 }
