37d47e9d46e407c2eaf6b4c919c466e5c094b469
romeo <romeoquist@gmail.com>
2023-12-18 15:22:10 -0600

    Added fields, constructors and a getter to the User class

--- src/main/java/TechDetectives/HobbyFinder/models/User.java
+++ src/main/java/TechDetectives/HobbyFinder/models/User.java
@@ -1,13 +1,31 @@
 package TechDetectives.HobbyFinder.models;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 
 //An entity represents a table stored in a database. Every instance of an
 //entity represents a row in the table. Once the database is set up, this
 //annotation can be used.
 
 //@Entity
-public class User {
+public class User extends AbstractEntity {
 
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 30, message = "Username must be 3-30 characters long")
+    private String username;
 
+    @NotBlank
+    private String pwHash;
 
+    public User() {}
+
+    public User(String username, String password){
+        this.username = username;
+        this.pwHash = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
 }
