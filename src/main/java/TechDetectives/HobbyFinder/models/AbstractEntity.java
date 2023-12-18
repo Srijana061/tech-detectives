214739d2a72f08185b19f5052a8f39d9b17a23b1
romeo <romeoquist@gmail.com>
2023-12-18 14:46:41 -0600

    Added id field, id getter, and equals method to AbstractEntity

--- src/main/java/TechDetectives/HobbyFinder/models/AbstractEntity.java
+++ src/main/java/TechDetectives/HobbyFinder/models/AbstractEntity.java
@@ -1,7 +1,31 @@
 package TechDetectives.HobbyFinder.models;
 
-public class AbstractEntity {
 
+//MappedSuperclass behaves similarly to the entity annotation but the class itself
+//does not map anything to a table in the database but only its subclasses/classes
+//that extend this class. This will be implemented once the database is set up.
 
+//@MappedSuperclass
+public abstract class AbstractEntity {
 
+    //Id will be used to indicate that the variable below is the primary key.
+
+    //GeneratedValue is used to assign a value to each primary key, a number that has not
+    //already been used.
+
+    //@Id
+    //@GeneratedValue
+    private int id;
+
+    public int getId(){
+        return id;
+    }
+
+    @Override
+    public boolean equals (Object o){
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractEntity entity = (AbstractEntity) o;
+        return id == entity.id;
+    }
 }
