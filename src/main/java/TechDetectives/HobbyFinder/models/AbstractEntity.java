9c49009fcae761217598fdb05b2868c2acc529ec
romeo <romeoquist@gmail.com>
2023-12-18 12:30:25 -0600

    Created the AbstractEntity class and wrote some comments in the User class

---
+++ src/main/java/TechDetectives/HobbyFinder/models/AbstractEntity.java
@@ -0,0 +1,7 @@
+package TechDetectives.HobbyFinder.models;
+
+public class AbstractEntity {
+
+
+
+}
--- src/main/java/TechDetectives/HobbyFinder/models/User.java
+++ src/main/java/TechDetectives/HobbyFinder/models/User.java
@@ -1,7 +1,13 @@
 package TechDetectives.HobbyFinder.models;
 
 
-//Entity can be used once database is set up
+//An entity represents a table stored in a database. Every instance of an
+//entity represents a row in the table. Once the database is set up, this
+//annotation can be used.
+
 //@Entity
 public class User {
+
+
+
 }
