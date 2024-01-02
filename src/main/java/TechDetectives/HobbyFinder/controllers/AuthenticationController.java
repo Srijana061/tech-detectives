83558894a3dfeae88afb1a2d99875e0e315e16a0
romeo <romeoquist@gmail.com>
2024-01-02 14:24:47 -0600

    Updated authentiation controller

--- src/main/java/TechDetectives/HobbyFinder/controllers/AuthenticationController.java
+++ src/main/java/TechDetectives/HobbyFinder/controllers/AuthenticationController.java
@@ -1,4 +1,39 @@
 package TechDetectives.HobbyFinder.controllers;
 
+import TechDetectives.HobbyFinder.data.UserRepository;
+import TechDetectives.HobbyFinder.models.User;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import java.util.Optional;
+
+@Controller
 public class AuthenticationController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    private static final String userSessionKey = "user";
+
+    public User getUserFromSession (HttpSession session){
+        Integer userId = (Integer) session.getAttribute(userSessionKey);
+        if (userId == null){
+            return null;
+        }
+
+        Optional<User> user = userRepository.findById(userId);
+
+        if(user.isEmpty()) {
+            return null;
+        }
+        return user.get();
+    }
+
+    private static void setUserInSession (HttpSession session, User user){
+        session.setAttribute(userSessionKey, user.getId());
+    }
+
+
+
 }
