8e6e974dc7f3ac5106887c43eadf57e1d94cbbec
romeo <romeoquist@gmail.com>
2024-01-14 20:28:35 -0600

    completed get mapping for the survey and most of survey.html

--- src/main/java/TechDetectives/HobbyFinder/Controllers/SurveyController.java
+++ src/main/java/TechDetectives/HobbyFinder/Controllers/SurveyController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.Collections;
 import java.util.List;
 
 @Controller
@@ -20,9 +21,15 @@ public class SurveyController {
     CategoryRepository categoryRepository;
 
     @GetMapping("/survey")
-    public String displaySurveyForm (Model model, Category category) {
+    public String displaySurveyForm (Model model) {
         List categories = (List<Category>) categoryRepository.findAll();
-        model.addAttribute("title","Sign Up");
+
+        if(categories.size() > 10){
+            categories = categories.subList(0,10);
+            Collections.shuffle(categories);
+        }
+
+        model.addAttribute("title","Sign Up Survey");
         model.addAttribute("categories", categories);
         return "survey";
     }
